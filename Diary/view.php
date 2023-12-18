fce268fc9e8a8bf296f8909eb8f3be0fb720ce47
qwqs78 <149639579+qwqs78@users.noreply.github.com>
2023-12-18 15:59:29 +0900

    Add files via upload

--- Diary/view.php
+++ Diary/view.php
@@ -1,67 +1,101 @@
 <?php
-require("db_connect.php");
+   $num = $_REQUEST["num"];
+   $page = $_REQUEST["page"] ?? 1;
 
-$num = $_GET["num"] ?? null;
-
-if (!$num) {
-    http_response_code(400);
-    header("Content-Type: application/json");
-    echo json_encode(["error" => "게시물 번호가 전달되지 않았습니다."]);
-    exit;
-}
-
-$query = $db->prepare("SELECT * FROM board WHERE num = ?");
-$query->execute([$num]);
-$post = $query->fetch(PDO::FETCH_ASSOC);
-
-if (!$post) {
-    http_response_code(404);
-    header("Content-Type: application/json");
-    echo json_encode(["error" => "해당 게시물을 찾을 수 없습니다."]);
-    exit;
-}
-
-header("Content-Type: application/json");
-echo json_encode(["post" => $post]);
+   require("db_connect.php"); //db접속
+   
+   $query = $db -> query("select * from board where num=$num");
+   if ($row = $query->fetch()){
+	   $writer = $row["writer"];
+	   $regtime = $row["regtime"];
+	   $picture = $row["picture"];
+	   $hits = $row["hits"];
+	   $action = "update.php?num=$num&page=$page";
+	   
+	   $title = str_replace(" ", "&nbsp", $row["title"]);
+	   $content = str_replace(" ", "&nbsp", $row["content"]);
+	   $content = str_replace("\n", "<br>", $content); //줄 띄우기?
+   }
+   if (isset($_GET["hits"])) {
+  $hits = $_GET["hits"];
+  $query = $db ->query("update board set hits = hits+1 where num=$num");
+} 
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>게시물 보기</title>
+    <meta charset="utf-8">
+    <style>
+        table { width: 800px; margin: 0 auto; text-align: center; } /* 테이블을 수평으로 가운데 정렬합니다. */
+        th    { width: 150px; background-color: pink; }
+        td    { text-align: center; border: 1px solid gray; padding: 5px; }
+		
+		.writer      { width:80px; }
+        .content    { width:50px; }
+		.cmt_id  {width:20px}
+		.writer {width: 20px}
+		
+		/* 버튼 위젯 가운데 정렬 스타일 */
+        .button-container {
+            text-align: center;
+            margin-top: 10px; /* 버튼 위젯과 상단 간격을 조절합니다. */
+        }
+    </style>
+	
 </head>
 <body>
-    <h1>게시물 보기</h1>
+<div style="text-align : center; margin-right:right;">
+<h1><a href="list.php">일기장</a></h1></div>
+<table>
+    <tr>
+        <th>작성자</th><td><?=$writer?></td>
+    </tr>
+    <tr>
+        <th>작성일시</th><td><?=$regtime?></td>
+    </tr>
+    <tr>
+        <th>조회수</th><td><?=$hits?></td>
+    </tr>
+    <tr>
+        <th>내용</th><td><?=$content?></td>
+    </tr>
+	<tr>
+    <th>사진</th> <td><img src="<?=$picture?>"></td>
+	</tr>
 
-    <div id="postDetails"></div>
+</table>
 
-    <script>
-        function fetchPostDetails(num) {
-            fetch(`view.php?num=${num}`)
-                .then(response => response.json())
-                .then(data => {
-                    const postDetails = document.getElementById('postDetails');
-                    const post = data.post;
+<div class="button-container">
+<input type="button" value="목록보기" onclick="location.href='list.php?page=<?=$page?>'">
+<input type="button" value="수정"     onclick="location.href='write.php?num=<?=$num?>&page=<?=$page?>'">
+<input type="button" value="삭제"     onclick="location.href='delete.php?num=<?=$num?>&page=<?=$page?>'"><br><br>
+</div>
+<?php
+   $page = $_REQUEST["page"] ?? 1;
+   
+    //새글 쓰기 모드
+   $title = "";
+   $writer = "";
+   $content = "";
+   $picture = "";
+   $action = "insert.php";
+   
+   //$num = isset($_REQUEST["num"]) ? $_REQUEST["num"] : 0;
+   $num = $_REQUEST["num"] ?? 0; //위에 의미랑 똑같음
+   
+   if ($num > 0){ //수정 모드로 셋팅
+       require("db_connect.php"); 
+       $query = $db -> query("select * from board where num=$num");
+	   
+       if ($row = $query->fetch()) {
+         $title = $row["title"];
+         $writer = $row["writer"];
+         $content = $row["content"];
+		 $picture = $row["picture"];
+         $action = "update.php?num=$num&page=$page";
+      }
+   }
+?>
 
-                    postDetails.innerHTML = `
-                        <table>
-                            <tr><th>작성자</th><td>${post.writer}</td></tr>
-                            <tr><th>작성일시</th><td>${post.regtime}</td></tr>
-                            <tr><th>조회수</th><td>${post.hits}</td></tr>
-                            <tr><th>내용</th><td>${post.content}</td></tr>
-                            <tr><th>사진</th><td><img src="${post.picture}"></td></tr>
-                        </table>
-                    `;
-                })
-                .catch(error => {
-                    console.error('Error fetching post details:', error);
-                });
-        }
-
-        // 페이지 로드 시 초기 데이터를 불러옴
-        const urlParams = new URLSearchParams(window.location.search);
-        const postNum = urlParams.get('num');
-        fetchPostDetails(postNum);
-    </script>
 </body>
 </html>
